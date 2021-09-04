e024b165ca8113c9490c381fc7c2e62474709b87
jeminsieow <sjemin7@gmail.com>
2021-09-05 03:46:06 +0800

    Add routes and models

--- server/index.js
+++ server/index.js
@@ -3,3 +3,21 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+// app.use(bodyParser.json({ limit: "30mb", extended: true }));
+// app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(express.json()) // To parse the incoming requests with JSON payloads
+app.use(express.urlencoded({extended: true}));
+app.use(cors());
+
+const CONNECTION_URL = 'mongodb+srv://sjemin:Password123@cluster0.hqhgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
