baef9e7fb092cd74bb71b6cf0c21d9d34823b486
jeminsieow <sjemin7@gmail.com>
2021-09-05 04:13:29 +0800

    Connect mongodb to express and add basic frontend structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
+import recipes from './images/recipes.png';
+import useStyles from './styles';
 
 const App = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Recipe App</h1>
-    </div>
+    <Container maxwindth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2">Recipeess</Typography>
+        <img className={classes.image} src={recipes} alt="recipes" height="200" />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cors());
 
 const CONNECTION_URL = 'mongodb+srv://sjemin:Password123@cluster0.hqhgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
