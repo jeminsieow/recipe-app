1cd09279548a7b58d5dbff29034c8237304b033d
jeminsieow <sjemin7@gmail.com>
2021-09-05 04:50:49 +0800

    Add Redux

--- server/index.js
+++ server/index.js
@@ -7,13 +7,13 @@ import postRoutes from './routes/posts.js'
 
 const app = express();
 
-app.use('/posts', postRoutes)
-
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(express.json()) // To parse the incoming requests with JSON payloads
 app.use(express.urlencoded({extended: true}));
 app.use(cors());
+
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL = 'mongodb+srv://sjemin:Password123@cluster0.hqhgm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
